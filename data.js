7818ef12674058dba61e01cf9e54678a7092a723
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-18 07:29:05 -0500

    Initial commit.

---
+++ data.js
@@ -0,0 +1,37 @@
+const data = {
+  todos: [
+    {
+      text: "make todo app better",
+      isComplete: true,
+      priority: 1
+    },
+    {
+      text: "finish the emoji movie project",
+      isComplete: true,
+      priority: 2
+    },
+    {
+      text: "catch up on netflix",
+      isComplete: false,
+      priority: 1
+    },
+    {
+      text: "read book for book club",
+      isComplete: false,
+      priority: 2
+    },
+    {
+      text: "internalize what a callback is",
+      isComplete: true,
+      priority: 1
+    },
+    {
+      text: "really get into callbacks",
+      isComplete: false,
+      priority: 1
+    }
+  ]
+}
+
+
+module.exports = data;
